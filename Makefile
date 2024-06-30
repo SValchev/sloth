3f97caff782bd18b9d792453a336c1cab0ae9250
Stanimir Valchev <x-stanimir.valchev@modelon.com>
2024-06-30 20:08:54 +0300

    chore: format

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ shell:
 .PHONY: format
 format:
 	poetry run -- ruff check --fix && ruff format 
+
+.PHONY: lint
+lint:
+	poetry run -- ruff check && mypy sloth test 
--- test/helpers.py
+++ test/helpers.py
@@ -1,10 +1,10 @@
-from typing import Tuple
-
 from sloth.lexer import Lexer
 from sloth.token import TokenType
 
+_expected_type = list[tuple[TokenType, str]]
 
-def validate_input(input_: str, expected: Tuple[TokenType, str]) -> None:
+
+def validate_input(input_: str, expected: _expected_type) -> None:
     lexer = Lexer(input_)
     for token_type, literal in expected:
         token = lexer.next_token()
